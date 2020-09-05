9decebb343290cadbfba2eacb4daed5cf5ad7dd0
Alejandro Martinez <almarag@gmail.com>
2020-09-05 13:12:23 -0500

    Added assembly

---
+++ AMA.Users/UsersAssemblyApplication.cs
@@ -0,0 +1,12 @@
+﻿namespace AMA.Users
+{
+    using System.Reflection;
+
+    public class UsersAssemblyApplication
+    {
+        public Assembly GetAssembly()
+        {
+            return typeof(UsersAssemblyApplication).GetTypeInfo().Assembly;
+        }
+    }
+}
